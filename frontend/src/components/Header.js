e09d7f3a87fcbb31fb4493d2ca75e659cd870faf
pushkarasharma <pushkarasharma11@gmail.com>
2021-07-10 18:37:17 +0530

    Reactify the links

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import {Navbar, Nav, Container, Row} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function Headers() {
     return (
         <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">GadgetBuster</Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand>GadgetBuster</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-                    <Nav.Link href="/login"><i className="fa fa-user"></i>Login</Nav.Link>
+                    <LinkContainer to="/cart">
+                        <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                    </LinkContainer>  
+                    <LinkContainer to="/login">
+                        <Nav.Link><i className="fa fa-user"></i>Login</Nav.Link>
+                    </LinkContainer>  
                     </Nav>
                 </Navbar.Collapse>
             </Container>
