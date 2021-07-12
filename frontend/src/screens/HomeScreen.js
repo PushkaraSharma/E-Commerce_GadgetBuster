743f20a3e1e7375119cb6db13d443913070446f1
pushkarasharma <pushkarasharma11@gmail.com>
2021-07-12 18:20:13 +0530

    added redux functionality

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,33 @@
 import React, {useState, useEffect} from 'react'
+import {useSelector,useDispatch} from 'react-redux'
 import products from '../products'
 import Product from '../components/Product'
 import { Row, Col} from 'react-bootstrap'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 function HomeScreen() {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error,loading,products} = productList
 
     useEffect(()=>{
-        async function fetchProducts(){
-            const {data} = await axios.get('/api/products/') 
-            setProducts(data)
-        }
+            dispatch(listProducts())
+    }, [dispatch])
 
-        fetchProducts()
-    }, [])
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
+            {loading ? <h2>Loading...</h2>
+            :error?<h3>{error}</h3>:
+                <Row>
                 {products.map(product =>(
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
                     </Col>
                 ))}
-            </Row>
+                </Row>
+            }
+
         </div>
 
     )
