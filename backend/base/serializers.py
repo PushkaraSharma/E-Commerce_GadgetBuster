5746c06c558ea023acbea7fd344e981a74536b96
pushkarasharma <pushkarasharma11@gmail.com>
2021-07-17 19:02:24 +0530

    JWT in backend and cart redux in frontend

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -2,9 +2,42 @@ from django.db import models
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['_id','name','email','isAdmin']
+
+    def get__id(self,object):
+        return object.id    
+
+    def get_isAdmin(self, object):
+        return object.is_staff    
+    
+    def get_name(self,object):
+        name = object.first_name
+        if name=='':
+            name = object.email
+
+        return name
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class UserSerializerWithToken(UserSerializer):
+    class Meta:
+        model = User
+        fields = ['_id','name','email','isAdmin','token'] 
+
+    def get_token(self, object):
+        token = RefreshToken.for_user(object)
+        return str(token)      
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView  
 
 urlpatterns = [
+    path('users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="getRoutes"),
+    path('users/profile', views.getUserProfile, name="getUserProfile"),
+    path('users', views.getUsers, name="getUsers"),
     path('products/', views.getProducts, name="getProducts"),
     path('products/<str:pk>', views.getProduct, name="getProduct"),
 ]
